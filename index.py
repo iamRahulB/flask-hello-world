48d382245cf415086caec260c14166d89dff6232
Rahul Bhole <108116259+iamRahulB@users.noreply.github.com>
2023-05-09 02:38:14 +0530

    Update index.py

--- index.py
+++ index.py
@@ -5,8 +5,7 @@ from image_generator import generate_image
 from flask import session
 from job_result import JobSuggestionResult
 
-app = Flask(__name__,static_folder='static')
-app.static_folder='static'
+app = Flask(__name__)
 app.secret_key = 'ihfheiufhiweuhf7efyw8eyf8ye4y4'
 
 openai.api_key = os.environ["OPENAI_API"]
