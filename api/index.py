cbb1d264339a02ebb492df72b64ecb6cc719121b
Rahul Bhole <108116259+iamRahulB@users.noreply.github.com>
2023-05-09 00:58:32 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from second import second_app
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+app.register_blueprint(second_app)
+
+if __name__ == '__main__':
+    app.run()
